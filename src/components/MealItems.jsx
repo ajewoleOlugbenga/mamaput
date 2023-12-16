101b13a8056f88b2d08cdfb1da5ee645031f3792
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2023-12-16 12:37:10 +0100

    adding a currency formatting to the meal.prices

--- src/components/MealItems.jsx
+++ src/components/MealItems.jsx
@@ -1,1 +1,21 @@
-rafce
+import { Formatter } from "../utils/Formatting"
+
+const MealItems = ({meal}) => {
+  return (
+    <li className='meal-item'>
+     <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
+            <h3>{meal.name}</h3>
+            <p className='meal-item-price'>{Formatter.format(meal.price)}</p>
+            <p className='meal-item-description'>{meal.description}</p>
+        </div>
+        <p className='meal-item-action'>
+            <button>Add to Cart</button>
+        </p>
+        </article> 
+    </li>
+  )
+}
+
+export default MealItems
