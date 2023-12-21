2e8fc5e270fc89726d33868767ba805e2b0ac45b
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2023-12-21 15:58:28 +0100

    formatting the item price with our formattr component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,7 +9,7 @@ const Cart = () => {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
   const cartTotal = cartCtx.items.reduce(
-    (totalPrice, item) => (totalPrice + item.quantity * item.price),
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
@@ -17,20 +17,41 @@ const Cart = () => {
     userProgressCtx.hideCart();
   }
 
+  function handleGoToCheckout() {
+    userProgressCtx.showCheckout();
+  }
+
   return (
-    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+    <Modal
+      className="cart"
+      open={userProgressCtx.progress === "cart"}
+      onClose={userProgressCtx.progress === "cart" ? handleCloseCart : null}
+    >
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
+          <li key={item.id} className="cart-item">
+            <p>
+              {item.name} - {item.quantity} × {Formatter.format(item.price)}
+            </p>
+            <p className="cart-item-actions">
+              <button onClick={() => cartCtx.removeItem(item.id)}>-</button>
+              <span>{item.quantity}</span>
+              <button onClick={() => cartCtx.addItem(item)}>+</button>
+            </p>
           </li>
         ))}
       </ul>
       <p className="cart-total">{Formatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly={true} onClick={handleCloseCart}>close</Button>
-        <Button textOnly={false} onClick={handleCloseCart}>Go to Checkout</Button>
+        <Button textOnly={true} onClick={handleCloseCart}>
+          close
+        </Button>
+        {cartCtx.items.length > 0 && (
+          <Button textOnly={false} onClick={handleGoToCheckout}>
+            Go to Checkout
+          </Button>
+        )}
       </p>
     </Modal>
   );
